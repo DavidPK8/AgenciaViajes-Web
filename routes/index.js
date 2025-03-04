1a8009a146f4bc4dc206715a28d2c051b095f484
DavidPK8 <alejodavicho8@gmail.com>
2025-03-04 18:45:29 -0500

    Implementacion de pug, creacion de router y views

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/', (req, res) => { // req - lo que enviamos, res - lo que express nos responde
+    res.send('Inicio');
+});
+
+router.get('/nosotros', (req, res) => { // req - lo que enviamos, res - lo que express nos responde
+    res.render('nosotros');
+});
+
+
+export default router;
