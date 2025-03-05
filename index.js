d39d68000e2713fded3f8f909ee37772e2fb3e4e
DavidPK8 <alejodavicho8@gmail.com>
2025-03-05 18:22:07 -0500

    Nosotros: Seccion Finalizada

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ const port = process.env.PORT || 4000;
 // Habilitar PUG
 app.set('view engine', 'pug');
 
+// Obtener el año actual
+app.use((req, res, next) => {
+    const year = new Date();
+    res.locals.year = year.getFullYear();
+    res.locals.nombreSitio = "Agencia de Viajes";
+
+    next();
+});
+
 // Definir la carpeta publica
 app.use(express.static('public'));
 
--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,28 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/', (req, res) => { // req - lo que enviamos, res - lo que express nos responde
-    res.render('inicio');
-});
-
-router.get('/nosotros', (req, res) => { // req - lo que enviamos, res - lo que express nos responde
-    const viajes = 'Viaje a Japon';
-    
-    res.render('nosotros', {
-        viajes
+router.get('/', (req, res) => { // req - lo que enviamos, res - lo que express nos responde 
+    res.render('inicio', {
+        pagina: 'Inicio'
     });
 });
 
+router.get('/nosotros', (req, res) => { 
+    res.render('nosotros', {
+        pagina: 'Nosotros'
+    });
+});
+
+router.get('/viajes', (req, res) => { 
+    res.render('viajes', {
+        pagina: 'Viajes'
+    });
+});
+
+router.get('/testimoniales', (req, res) => { 
+    res.render('testimoniales', {
+        pagina: 'Testimoniales'
+    });
+});
 
 export default router;
