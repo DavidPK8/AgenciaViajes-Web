62b77cbf3aa172d924b327c0212c9e06779a90bc
DavidPK8 <alejodavicho8@gmail.com>
2025-03-11 19:08:20 -0500

    Deployment en Render

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import router from './routes/index.js';
 import db from './config/db.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
@@ -34,5 +37,5 @@ app.use(express.static('public'));
 app.use('/', router);
 
 app.listen(port, () => {
-    console.log(`El servidor esta funcionando en el puerto ${port}`)
+    console.log(`El servidor esta funcionando`)
 });
